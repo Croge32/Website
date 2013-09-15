7f492ce98b3f0c8223992c75f9ffa0cf796f4b71
Croge32 <croge32@tigers.lsu.edu>
2013-09-14 23:06:35 -0500

    Delete header.php

--- html and php/header.php
+++
@@ -1,6 +0,0 @@
-<div id="headerbar">
-	<img id="headerbarImage" src="images/webbanner.jpg" usemap="#tvmap"/>
-		<map name="tvmap">
-			<area shape="rect" coords="156, 69, 879, 114" href="home.html" alt="Click for home page">
-		</map>
-</div>

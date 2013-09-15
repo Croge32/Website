8f9903ca224b956e2fba5166c210c59193df04aa
Croge32 <croge32@gmail.com>
2013-09-14 21:57:11 -0500

    again

---
+++ css/header.php
@@ -0,0 +1,6 @@
+<div id="headerbar">
+	<img id="headerbarImage" src="images/webbanner.jpg" usemap="#tvmap"/>
+		<map name="tvmap">
+			<area shape="rect" coords="156, 69, 879, 114" href="home.html" alt="Click for home page">
+		</map>
+</div>
---
+++ test.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include ('headings.php'); ?>
+</head>
+<body></body>
+<html>

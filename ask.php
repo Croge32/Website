067c71d19d7c5e9b17740d233f2ce03547b1202e
Croge32 <croge32@gmail.com>
2013-09-17 09:10:39 -0500

    php email form fix?

--- ask.php
+++ ask.php
@@ -5,30 +5,33 @@
 
 	<h2 id="askheader"> Questions? Comments? Reviews? Email us here.</h2>
 
-	<div id="askform" <?php
-		if (isset($_REQUEST['email']))
+	<div id="askform">
+		<?php
+			if (isset($_REQUEST['email']))
 //if "email" is filled out, send email
- 		{
- //send email
-  			$email = $_REQUEST['email'] ;
- 			$subject = $_REQUEST['subject'] ;
-  			$message = $_REQUEST['message'] ;
-  			mail("croge32@gmail.com", $subject,
-  			$message, "From:" . $email);
- 			echo "Thank you for using our mail form";
- 		}
-		else
+  			{
+  //send email
+  				$email = $_REQUEST['email'] ;
+  				$subject = $_REQUEST['subject'] ;
+  				$message = $_REQUEST['message'] ;
+  				mail("croge32@gmail.com", $subject,
+  				$message, "From:" . $email);
+  				echo "Thank you for using our mail form";
+			}
+			else
 //if "email" is not filled out, display the form
-		{
- 			echo "<form method='post' action='mailform.php'>
-			Email: <input id='emailbox' name='email' type='text'><br>
- 		 	Subject: <input style='margin-bottom: 1em;' name='subject' type='text'><br>
- 		 	Message:<br>
-		 	<textarea name='message' rows='15' cols='120'></textarea><br>
- 		 	<input type='submit'>
- 		 	</form>";
-  		}
-	?></div>
+  			{
+  				echo "<form method='post' action='mailform.php'>
+  				Email: <input name='email' type='text'><br>
+  				Subject: <input name='subject' type='text'><br>
+  				Message:<br>
+  				<textarea name='message' rows='15' cols='40'>
+  				</textarea><br>
+  				<input type='submit'>
+  				</form>";
+  			}
+		?>
+	</div>
 
 	<?php include('footer.php'); ?>
 	

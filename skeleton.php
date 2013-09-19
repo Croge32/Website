8831ae6b101bb4a7e3663018e818df5faa310247
Croge32 <croge32@gmail.com>
2013-09-19 01:25:20 -0500

    some formatting experiments

--- skeleton.php
+++ skeleton.php
@@ -5,7 +5,7 @@
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
 	<title>Affordable In-Home TV Service | Broken TV? We Come to You!</title>
-	<meta name="Affordable In-Home TV Service" content="Broken TV? Don't take it to the shop! Call us at (225) 262-4473 and we'll come to you!">
+	<meta name="Affordable In-Home TV Service" content="Broken TV? Don't take it to the shop! Call us at (225) 262-4473 and we'll come to you!"
 </head>
 <body>
 	<?php include_once("analyticstracking.php") ?>
@@ -18,7 +18,6 @@
 			</map>
 		</div>
 
-		<div id="rightbanner">
 			<div class="sidebuttons">
 				<a class="buttonlinks" href="home.php">Home</a>
 			</div>
@@ -34,4 +33,3 @@
 			<div class="sidebuttons">
 				<a class="buttonlinks" href="ask.php">Ask Us a Question!</a>
 			</div>
-		</div>
